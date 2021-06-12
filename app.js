1440462e1e429222367461fea893e0ed173b38e6
Adam Saher <adamsaher@Adams-MacBook-Pro.local>
2021-06-12 13:49:03 -0400

    exphbs

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const exphbs = require('express-handlebars')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+
+app.engine('hbs', exphbs({
+  extname: 'hbs',
+  helpers: {
+    increment: (num) => {
+      return Number(num) + 1
+    },
+    decrement: (num) => {
+      return Number(num - 1)
+    },
+    random: () => {
+      return Math.floor(Math.random() * (2500 - 1) + 1)
+    }
+  }
+}))
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -23,12 +38,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
